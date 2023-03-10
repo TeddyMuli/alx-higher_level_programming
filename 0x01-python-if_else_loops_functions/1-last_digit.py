c9c6496f44821130eeead6f9c1e4126a51651c3f
TeddyMuli <tmuli974@gmail.com>
2023-03-10 07:28:48 -0800

    new

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,5 +7,5 @@ if last > 5:
     print("and is greater than 5")
 if last == 0:
     print("and is 0")
-if last < -6 && last != 0:
+if last < -6 and last != 0:
     print("and is less than 6 and not 0")
