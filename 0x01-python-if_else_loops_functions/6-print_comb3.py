ee189e209679114d2102601ee2e1cd4658065185
TeddyMuli <tmuli974@gmail.com>
2023-03-13 06:11:43 +0000

    last chech

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,5 @@ for i in range(10):
         if ((i != j) and (i < j)):
             if (i < 8):
                 print("{:d}{:d}, ".format(i, j), end='')
-        else:
+            else:
                 print("{:d}{:d}".format(i, j), end='\n')
