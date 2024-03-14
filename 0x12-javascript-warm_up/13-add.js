32e168f914a82ab59b4d42e3db145c28bb6f0724
TeddyMuli <tmuli974@gmail.com>
2024-03-14 15:13:15 +0300

    0x12-javascript-warm_up

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const add = (a, b) => {
+    return parseInt(a) + parseInt(b);
+}
+
+exports.add = add;
---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+
+let number = parseInt(arg);
+
+if (isNaN(number)) {
+    console.log("Missing size");
+} else {
+    for (let i = 0; i < number; i++) {
+        let row = ""
+        for (let j = 0; j < number; j++) {
+            row += "X";
+        }
+        console.log(row)
+    }
+}
